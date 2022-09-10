e78b47e03b2c996012c2bf77aed959272684ec5c
AzkSumWave36 <74044050+azkazunami36@users.noreply.github.com>
2022-09-10 11:46:14 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,100 +1,85 @@
-function sound() { document.getElementById('memz').play(); };
+function sound() { document.getElementById('memz').play(); }; //再生
+
+var //数が多いほどランダム性が上がると思います
+image1 = 280, //確率、img.png
+image2 = 10, //確率、nyancat.gif
+image3 = 10, //確率、miku.gif
+playaudio = "nyancata.mp3"; //再生する音声ファイル
+var x, y; //ドラッグ時に使用すると思われる変数
 
 window.onload = function () {
+    document.getElementById("memz").src = playaudio;
     var bodyid = document.getElementById("bodyi");
-    for (let i = 0; i != 462; i++) {
+    let fors = 0;
+    interval = setInterval(() => {
+        if (fors > 500) clearInterval(interval);
         var bo0 = document.createElement("div");
         var bo1 = document.createElement("img");
-        bo0.className = "drag-and-drop";
-        bo0.style.top = "110px";
-        bo0.style.left = "110px";
-        bo0.style.display = "none";
-        bo0.id = "a" + i;
-        var types = Math.floor(Math.random() * 300);
-        if (types < 280) {
+        var sW = Math.floor(Math.random() * (document.documentElement.clientWidth - 90));
+        var sH = Math.floor(Math.random() * (document.documentElement.clientHeight - 90));
+        bo0.style.top = sH + "px";
+        bo0.style.left = sW + "px";
+        bo0.style.cursor = "move";
+        bo0.style.width = "7%";
+        bo0.style.position = "absolute";
+        bo0.style.zIndex = "1000";
+        var types = Math.floor(Math.random() * (image1 + image2 + image3));
+        if (types < image1) {
             bo1.src = "img.png";
-        } else if (types < 295) {
+        } else if (types < (image2 + image1)) {
             bo1.src = "cat.gif";
-        } else {
-            bo1.src = "初音ミク.gif";
-        }
+        } else if (types < (image3 + image2 + image1)) {
+            bo1.src = "miku.gif";
+        };
         bo1.style.width = "100%";
         bo1.onclick = sound;
         bo0.appendChild(bo1);
         bodyid.appendChild(bo0);
-    };
-    window_load();
-    var elements = document.getElementsByClassName("drag-and-drop");
-    var x, y;
-    for (var i = 0; i < elements.length; i++) {
-        elements[i].addEventListener("mousedown", mdown, false);
-        elements[i].addEventListener("touchstart", mdown, false);
-    };
-    function mdown(e) {
-        var bo0 = document.createElement("audio");
-        bo0.src = "nyancata.mp3";
-        bo0.style.display = "none";
-        bo0.autoplay = true;
-        bo0.loop = true;
-        this.classList.add("drag");
-        if (e.type === "mousedown") {
-            var event = e;
-        } else {
-            var event = e.changedTouches[0];
-        };
-        x = event.pageX - this.offsetLeft;
-        y = event.pageY - this.offsetTop;
-        document.body.addEventListener("mousemove", mmove, false);
-        document.body.addEventListener("touchmove", mmove, false);
-    };
-    function mmove(e) {
-        document.addEventListener('touchmove', function (event) { event.preventDefault(); }, { passive: false });
-        var drag = document.getElementsByClassName("drag")[0];
-        if (e.type === "mousemove") {
-            var event = e;
-        } else {
-            var event = e.changedTouches[0];
-        };
-        e.preventDefault();
-        drag.style.top = event.pageY - y + "px";
-        drag.style.left = event.pageX - x + "px";
-        drag.addEventListener("mouseup", mup, false);
-        document.body.addEventListener("mouseleave", mup, false);
-        drag.addEventListener("touchend", mup, false);
-        document.body.addEventListener("touchleave", mup, false);
-        document.removeEventListener('touchmove', handleTouchMove, { passive: false });
-    };
-    function mup(e) {
-        var drag = document.getElementsByClassName("drag")[0];
-        document.body.removeEventListener("mousemove", mmove, false);
-        drag.removeEventListener("mouseup", mup, false);
-        document.body.removeEventListener("touchmove", mmove, false);
-        drag.removeEventListener("touchend", mup, false);
-        drag.classList.remove("drag");
-    };
-    document.getElementById("intro").addEventListener("ended", () => {
-        sound();
-    });
+        bo0.addEventListener("mousedown", mdown, false);
+        bo0.addEventListener("touchstart", mdown, false);
+        fors++;
+    }, 50);
+    document.getElementById("intro").addEventListener("ended", () => { sound(); });
 };
-async function window_load() {
-    var sW, sH, s;
-    sW = document.documentElement.clientWidth - 110;
-    sH = document.documentElement.clientHeight - 100;
-    let esW, esH;
-    var elements, i = 0;
-    for (i = 0; i < 461; i++) {
-        elements = document.getElementById("a" + i);
-        esW = Math.floor(Math.random() * sW);
-        esH = Math.floor(Math.random() * sH);
-        elements.style.top = esH + "px";
-        elements.style.left = esW + "px";
-        await wait2();
-        elements.style.display = "block";
+
+function mdown(e) {
+    var bo0 = document.createElement("audio");
+    bo0.src = playaudio;
+    bo0.style.display = "none";
+    bo0.autoplay = true;
+    bo0.loop = true;
+    this.classList.add("drag");
+    if (e.type === "mousedown") {
+        var event = e;
+    } else {
+        var event = e.changedTouches[0];
     };
+    x = event.pageX - this.offsetLeft;
+    y = event.pageY - this.offsetTop;
+    document.body.addEventListener("mousemove", mmove, false);
+    document.body.addEventListener("touchmove", mmove, false);
 };
-function wait2() {
-    return new Promise(resolve => { setTimeout(resolve, 50) });
+function mmove(e) {
+    document.addEventListener('touchmove', function (event) { event.preventDefault(); }, { passive: false });
+    var drag = document.getElementsByClassName("drag")[0];
+    if (e.type === "mousemove") {
+        var event = e;
+    } else {
+        var event = e.changedTouches[0];
+    };
+    e.preventDefault();
+    drag.style.top = event.pageY - y + "px";
+    drag.style.left = event.pageX - x + "px";
+    drag.addEventListener("mouseup", mup, false);
+    document.body.addEventListener("mouseleave", mup, false);
+    drag.addEventListener("touchend", mup, false);
+    document.body.addEventListener("touchleave", mup, false);
 };
-function wait4() {
-    return new Promise(resolve => { setTimeout(resolve, 3800) });
+function mup(e) {
+    var drag = document.getElementsByClassName("drag")[0];
+    document.body.removeEventListener("mousemove", mmove, false);
+    drag.removeEventListener("mouseup", mup, false);
+    document.body.removeEventListener("touchmove", mmove, false);
+    drag.removeEventListener("touchend", mup, false);
+    drag.classList.remove("drag");
 };
